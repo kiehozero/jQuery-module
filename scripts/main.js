b0de4289660047ba24a40036d48c4e180cb30c00
Stuart Saint <stuartsaint242@gmail.com>
2020-09-09 18:34:55 +0000

    Progress up to callbacks lesson

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 var xhr = new XMLHttpRequest();
+var data;
+
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+xhr.send();
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+        data = JSON.parse(this.responseText);
+        };
     };
-};
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-
-xhr.send();
+setTimeout(function() {
+    console.log(data)
+}, 500);
